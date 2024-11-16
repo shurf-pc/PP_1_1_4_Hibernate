53faea69a2ca1056d2b1e88d07ff8efced4b63b7
Pavel <shurf.pc@gmaiil.com>
2024-11-16 09:50:46 +0300

    Hibernate 1st commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,24 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final Session session;
+
+    {
+        try {
+            session = Util.getHibernateSession();
+        } catch (HibernateException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public UserDaoHibernateImpl() {
 
     }
@@ -12,31 +26,51 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        Transaction tx = session.beginTransaction();
+        session.createSQLQuery("CREATE TABLE IF NOT EXISTS users (id int NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), lastName VARCHAR(50), age int)").executeUpdate();
+        tx.commit();
     }
 
     @Override
     public void dropUsersTable() {
-
+        Transaction tx = session.beginTransaction();
+        session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
+        tx.commit();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Transaction transaction = session.beginTransaction();
+        User user = new User();
+        user.setName(name);
+        user.setLastName(lastName);
+        user.setAge(age);
+        session.save(user);
+        transaction.commit();
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Transaction transaction;
+        User user = session.get(User.class, id);
+        if (user != null) {
+            transaction = session.beginTransaction();
+            session.delete(user);
+            transaction.commit();
+        } else {
+            System.out.println("User not found!");
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        return session.createQuery("from users", User.class).getResultList();
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Transaction tx = session.beginTransaction();
+        session.createSQLQuery("truncate table users").executeUpdate();
+        tx.commit();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,16 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 
 public class Util {
@@ -10,9 +18,29 @@ public class Util {
     private static final String DB_NAME = "learningsql";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        Properties prop = new Properties();
+        prop.setProperty(Environment.URL, "jdbc:mysql://" + HOST_NAME + ":3306/" + DB_NAME);
+        prop.setProperty(Environment.USER, USER);
+        prop.setProperty(Environment.PASS, PASSWORD);
+        prop.setProperty(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        prop.setProperty(Environment.DRIVER, DRIVER);
+        Configuration config = new Configuration();
+        config.setProperties(prop);
+        config.addAnnotatedClass(User.class);
+        sessionFactory = config.buildSessionFactory();
+    }
 
     public static Connection getMySQLConnection() throws SQLException {
         return DriverManager.getConnection("jdbc:mysql://" + HOST_NAME + ":3306/" + DB_NAME, USER, PASSWORD);
+    }
+
+    public static Session getHibernateSession() throws HibernateException {
+        return sessionFactory.openSession();
     }
 }
 
